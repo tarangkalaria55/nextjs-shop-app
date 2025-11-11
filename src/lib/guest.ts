3f2f82f97808637f2de31eeca93837c730946da5
Tarang Kalariya <tarangkalaria55@gmail.com>
2025-11-11 07:25:27 +0530

    -

--- src/lib/guest.ts
+++ src/lib/guest.ts
@@ -6,16 +6,20 @@ const GUEST_ID_COOKIE = "guest-id";
 
 export const getGuestId = async () => {
   const cookieStore = await cookies();
-  let guestId = cookieStore.get(GUEST_ID_COOKIE)?.value;
-  if (!guestId) {
-    guestId = crypto.randomUUID(); // Generate a unique ID
-    cookieStore.set(GUEST_ID_COOKIE, guestId, {
-      httpOnly: true, // Secure, not accessible via JS
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 60 * 60 * 24 * 30, // 30 days
-      path: "/",
-    });
-  }
+  const guestId =
+    cookieStore.get(GUEST_ID_COOKIE)?.value || (await createGuestId());
+  return guestId;
+};
+
+export const createGuestId = async () => {
+  const cookieStore = await cookies();
+  const guestId = crypto.randomUUID(); // Generate a unique ID
+  cookieStore.set(GUEST_ID_COOKIE, guestId, {
+    httpOnly: true, // Secure, not accessible via JS
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+    path: "/",
+  });
   return guestId;
 };
 
